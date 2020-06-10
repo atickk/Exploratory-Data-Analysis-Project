09e357f15b54e5a31b38ef6ae8f80d0fd59553fa
Atiq ur Rehman <48656175+atickk@users.noreply.github.com>
2020-06-11 04:20:55 +0500

    Add files via upload

---
+++ Plot 3.R
@@ -0,0 +1,14 @@
+projectdata <- read.table("C:/Users/atiq.rehman/Desktop/Data Science Practice/Exploratory data/Project 1/Household Power Consumption/household_power_consumption.txt", skip = 1, sep = ";")
+names(projectdata) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+subsetofprojectdata <- projectdata[projectdata$Date %in% c("1/2/2007","2/2/2007") ,]
+globalactivepower <- as.numeric(subsetofprojectdata$Global_active_power)
+dateandtime <- strptime(paste(subsetofprojectdata$Date, subsetofprojectdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+
+submetering1 <- as.numeric(subsetofprojectdata$Sub_metering_1)
+submetering2 <- as.numeric(subsetofprojectdata$Sub_metering_2)
+submetering3 <- as.numeric(subsetofprojectdata$Sub_metering_3)
+
+plot(dateandtime, submetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(dateandtime, submetering2, type="l", col="red")
+lines(dateandtime, submetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
